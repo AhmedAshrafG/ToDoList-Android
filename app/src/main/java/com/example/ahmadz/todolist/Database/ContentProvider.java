53c563869aa5fe500a6534b4a07056b89f91a951
ahmadz <ahmed.ashraf33@yahoo.com>
2016-06-05 22:03:23 +0200

    Added a content provider to interface the DB.

---
+++ app/src/main/java/com/example/ahmadz/todolist/Database/ContentProvider.java
@@ -0,0 +1,39 @@
+package com.example.ahmadz.todolist.Database;
+
+import android.content.Context;
+import com.example.ahmadz.todolist.Models.TodoItemModel;
+import java.util.List;
+import rx.Observable;
+
+/**
+ * Created by ahmadz on 6/5/16.
+ */
+
+public class ContentProvider {
+
+	private MyDBHelper dbHelper;
+	private static ContentProvider mInstance;
+
+	public static ContentProvider getInstance(Context context){
+		if (mInstance == null)
+			mInstance = new ContentProvider(context);
+
+		return mInstance;
+	}
+
+	private ContentProvider(Context mContext){
+		dbHelper = MyDBHelper.getInstance(mContext);
+	}
+
+	public Observable<List<TodoItemModel>> getTodoListObservable(){
+		return Observable.create(subscriber -> {
+			try{
+				subscriber.onNext(dbHelper.getAllTodoItems());
+				subscriber.onCompleted();
+
+			}catch (Exception e){
+				subscriber.onError(e);
+			}
+		});
+	}
+}
